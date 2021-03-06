2b3074ee6e6654d7f8357302052bb40f2585d5eb
Kevin Sajan <kevsaj@gmail.com>
2021-03-06 17:53:57 -0500

    styling

--- src/views/Index.js
+++ src/views/Index.js
@@ -9,17 +9,23 @@ import Wrapper from "../components/Wrapper";
 import Title from "../components/Title";
 import friends from "../components/friends.json";
 
+import CardTable from "components/Cards/CardTable.js";
+
+import HeaderStats from "components/Headers/HeaderStats.js";
+
 export default function Index() {
 
 
 
   return (
     <>
+    <main>
       <Wrapper>
         <Title>Employee Directory</Title>
       </Wrapper>
+      
 
-      <section className="header relative pt-2 items-center flex h-screen max-h-860-px">
+      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
         <div className="container mx-auto items-center flex flex-wrap">
           <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
             <div className="pt-32 sm:pt-0">
@@ -39,11 +45,24 @@ export default function Index() {
         </div>
 
         <img
-          className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-720px"
-          src={require("assets/img/pattern_react.png")}
+          className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860px"
+          src={require("../assets/img/pattern_react.png")}
           alt="..."
         />
       </section>
+
+      <div className="flex flex-wrap mt-4">
+        <div className="w-full mb-12 px-4">
+          <CardTable color="dark" />
+        </div>
+      </div>
+
+      <HeaderStats />
+    </main>
+
+      <Footer />
+
+      
     </>
   );
 }
