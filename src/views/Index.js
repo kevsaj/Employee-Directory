e5beae3a82ed62b5bd62ace5582f36ffb97f726c
Kevin Sajan <kevsaj@gmail.com>
2021-03-07 22:36:12 -0500

    fixed errors

--- src/views/Index.js
+++ src/views/Index.js
@@ -34,8 +34,8 @@ export default function Index() {
               </h2>
               <p className="mt-4 text-lg leading-relaxed text-gray-600">
                 <div class="relative flex w-full flex-wrap items-stretch mb-3">
-                  <input type="text" placeholder="Large Input" class="px-3 py-4 placeholder-gray-400 text-gray-700 relative bg-white bg-white rounded text-base border border-gray-400 outline-none focus:outline-none focus:shadow-outline w-full pr-10"/>
-                  <span class="z-10 h-full leading-normal font-normal absolute text-center text-gray-400 absolute bg-transparent rounded text-lg items-center justify-center w-8 right-0 pr-3 py-4">
+                  <input type="text" placeholder="Large Input" class="px-3 py-4 placeholder-gray-400 text-gray-700 relative bg-white rounded text-base border border-gray-400 outline-none focus:outline-none focus:shadow-outline w-full pr-10"/>
+                  <span class="z-10 h-full leading-normal font-normal text-center text-gray-400 absolute bg-transparent rounded text-lg items-center justify-center w-8 right-0 pr-3 py-4">
                     <i class="fas fa-user"></i>
                   </span>
                 </div>
