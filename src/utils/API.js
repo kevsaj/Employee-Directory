1be3e476376b219f4cf0acb592f5f2cd6f2a58d2
Kevin Sajan <kevsaj@gmail.com>
2021-03-10 21:30:19 -0500

    rebuilding app

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API = {
-    search: function () {
-        return axios.get('https://randomuser.me/api/?results=10&inc=id,picture,name,cell,email,dob,location&nat=ca,us')
-    }
-}
-
-export default API;
